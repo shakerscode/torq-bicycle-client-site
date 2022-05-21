5db7670689c0c4ec27709d5ff7d7a07511d5c919
Shaker Ahamed <ahamedshaker7@gmail.com>
2022-05-22 00:41:30 +0600

    products section done

--- src/Pages/HomePage/CycleParts.js
+++ src/Pages/HomePage/CycleParts.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import PrimaryBtn from '../SharedPages/PrimaryBtn';
 
 const CycleParts = ({ part }) => {
+    const navigate = useNavigate()
+    const {name, image, shortDesc, minOrder, availableQuantity, price} = part;
     return (
-        <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card max-w-sm shadow-xl">
             <figure class="px-10 pt-10">
-                <img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" class="rounded-xl" />
+                <img src={image} alt="products" class="rounded-xl" />
             </figure>
             <div class="card-body items-center text-center">
-                <h2 class="card-title">{part.name}</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions">
-                    <button class="btn btn-primary">Buy Now</button>
+                <h2 class="card-title">{name}</h2>
+                <p className='text-gray-500'>{shortDesc.slice(0, 100)}...</p>
+                <p className='text-md'>Min. Order: {minOrder} piece</p>
+                <p>Available: {availableQuantity}</p>
+                <p>Per Pice: {price}$</p>
+                <div onClick={()=> navigate('/')} class="card-actions">
+                    <PrimaryBtn>Buy Now</PrimaryBtn>
                 </div>
             </div>
         </div>
